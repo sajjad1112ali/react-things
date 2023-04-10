b88a88453839ced68a8c5704c892c0078f85b335
Sajjad Ali <sajjad.ali@arpatech.com>
2023-04-10 17:14:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      HELLO WORLD
+    </div>
+  );
+}
+
+export default App;
